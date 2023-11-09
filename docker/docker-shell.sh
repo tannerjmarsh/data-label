e340b93e8d53704b8600e8890ba9d565e6e7bc1f
Tanner Marsh <tam997@g.harvard.edu>
2023-11-09 15:04:16 -0500

    added nginx container to docker compose file

--- docker/docker-shell.sh
+++ docker/docker-shell.sh
@@ -6,12 +6,15 @@ set -e
 docker network inspect data-labeling-network >/dev/null 2>&1 || docker network create data-labeling-network
 
 # Build the image based on the Dockerfile
-docker build -t data-label-cli --platform=linux/arm64/v8 -f ../data-labeling-cli/Dockerfile ../data-labeling-cli
+docker build -t tanner3030/data-label-cli -f ../data-labeling-cli/Dockerfile ../data-labeling-cli
+# docker push tanner3030/data-label-cli
 
 # remove existing docker volumes if desired
 if [[ "$1" == "--fresh" ]]; then
     rm -r docker-volumes
 fi
 
+# docker pull tanner3030/data-label-cli
+ 
 # Run All Containers
-docker compose run --rm --service-ports data-label-cli
+# docker compose run --rm --service-ports data-label-cli
